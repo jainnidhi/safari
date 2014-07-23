256adb3da28315fd3dc9810d21db8dddfbd856c3
jainnidhi <jainnidhi_3@ymail.com>
2014-07-23 16:24:54 +0530

    modified styling for comment block and added site content nav

--- single.php
+++ single.php
@@ -33,6 +33,7 @@ get_header(); ?>
 	<?php while ( have_posts() ) : the_post(); ?>
 
 		<?php get_template_part( 'content', 'single' ); ?>
+                            <?php safari_content_nav( 'nav-below' ); ?>
 
                 <?php
 			// If comments are open or we have at least one comment, load up the comment template
